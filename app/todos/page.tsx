d8670f40f5dd489e38655e4c5121b045234c331d
yuya <kinjo.bobchin@gmail.com>
2023-11-01 18:34:52 +0900

    refactor: Simplify Todos page component
    The code changes in this commit refactor the Todos page component to simplify its structure. The previous implementation included unnecessary code for fetching data from an API and rendering a list of todos. This functionality has been removed as it is not currently needed. Instead, the component now renders a simple heading for the ToDo App.

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,29 +1,7 @@
-import Link from 'next/link';
-import data from '../../data.json';
-
-type Todos = typeof data.todos;
-
-async function getTodos() {
-  const res = await fetch('http://localhost:3001/todos');
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json() as Promise<Todos>;
-}
-
-export default async function Todos() {
-  const data = await getTodos();
+export default function Page() {
   return (
-    <>
-      <Link href="/">Homeに戻る</Link>
-      {data.map(({ id, title, completed }) => (
-        <div key={id}>
-          <h2>{title}</h2>
-          <p>{completed}</p>
-        </div>
-      ))}
-    </>
+    <div>
+      <h1>ToDo App</h1>
+    </div>
   );
 }
